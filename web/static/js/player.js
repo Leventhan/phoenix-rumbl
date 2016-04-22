bebe93becfef418b8a3510a703a0adc24216f4ef
Yos Riady <yosriady@gmail.com>
2016-04-23 01:29:22 +0800

    Basic channel join, receive, broadcast

--- web/static/js/player.js
+++ web/static/js/player.js
@@ -1,8 +1,8 @@
 let Player = {
   player: null,
 
-  init(domId, playerId, onReady) {
-    window.onYoutubeIframeAPIReady = {} => {
+  init(domId, playerId, onReady){
+    window.onYouTubeIframeAPIReady = () => {
       this.onIframeReady(domId, playerId, onReady)
     }
     let youtubeScriptTag = document.createElement("script")
@@ -10,21 +10,20 @@ let Player = {
     document.head.appendChild(youtubeScriptTag)
   },
 
-  onIframeReady(domId, playerId, onReady) {
+  onIframeReady(domId, playerId, onReady){
     this.player = new YT.Player(domId, {
       height: "360",
       width: "420",
       videoId: playerId,
       events: {
-        "onReady": (event => onReady(event)),
-        "onStateChange": (event => this.onPlayerStateChange(event))
+        "onReady":  (event => onReady(event) ),
+        "onStateChange": (event => this.onPlayerStateChange(event) )
       }
     })
   },
 
   onPlayerStateChange(event){ },
-  getCurrentTime(){ return Math.floor(this.player.getCurrentTime() * 1000) },  seekTo(millsec){ return this.player.seekTo(millsec / 1000) }
-
+  getCurrentTime(){ return Math.floor(this.player.getCurrentTime() * 1000) },
+  seekTo(millsec){ return this.player.seekTo(millsec / 1000) }
 }
-
 export default Player
